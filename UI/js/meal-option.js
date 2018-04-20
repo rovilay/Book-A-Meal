e3ca52e8748da465c33951390336474cd94ae3b4
rovilay <rovilayjnr01@gmail.com>
2018-04-20 12:24:33 +0100

    Update pages and links

--- UI/js/meal-option.js
+++ UI/js/meal-option.js
@@ -20,7 +20,8 @@ const UICtrl = (function () {
     displayTitle: '#display-title',
     mealList: '#meal-list',
     mealCount: '#meal-count',
-    clearBtn: '#clear-btn'
+    clearBtn: '#clear-btn',
+    mealCard: '.meal-card'
   };
 
   // Public methods
@@ -28,50 +29,6 @@ const UICtrl = (function () {
 
     getUISelectors: function () {
       return UISelectors;
-    },
-
-    populateMealDisplay: function (mealOptions) {
-      let html = ``;
-      if (mealOptions.length > 0) {
-        let today = new Date().toISOString().substr(0, 10);
-        mealOptions.forEach(meal => {
-          if(meal.date === today ) {
-          html += `<div class="card mb-1 lightGrey edit-mea" id="meal-${meal.id}">
-            <span class="h4 edit-meal"><b class='meal-name'>${meal.mealName}</b> &nbsp &nbsp &nbsp  &nbsp ${meal.date}</span>
-        </div>`
-          }
-        });
-      }
-
-      if(html !== ``) {
-        document.querySelector(UISelectors.displayTitle).style.display = 'block';
-      } else {
-        document.querySelector(UISelectors.displayTitle).style.display = 'none';  
-      }
-
-      // Insert list items
-      document.querySelector(UISelectors.mealList).innerHTML = html;
-
-      // Update contact count
-      this.updateMealCount();
-    },
-
-    updateMealCount: function () {
-
-      let mealOptions = MealCtrl.getData();
-      if (mealOptions.length !== 0) {
-        let count = 0;
-        let today = new Date().toISOString().substr(0, 10);
-        mealOptions.forEach(meal => {
-          if(meal.date === today) {
-            count += 1
-          }
-
-        })
-        document.querySelector(UISelectors.mealCount).innerText = count;
-      } else {
-        document.querySelector(UISelectors.mealCount).innerText = 0;
-      }
     },
 
     getFormInputs: function () {
@@ -98,18 +55,6 @@ const UICtrl = (function () {
       }, 5000);
     },
 
-    validateForm: function () {
-      const form = this.getFormInputs();
-
-      if (form.date === "" || form.meal === "") {
-        this.showAlert(UISelectors.alert, 'Please fill all fields!', 'alert alert-danger');
-        return false;
-      } else {
-
-        return true;
-      }
-    },
-
 
     hideMealListHeader: function () {
       document.querySelector(UISelectors.displayTitle).style.display = 'none';
@@ -125,10 +70,6 @@ const UICtrl = (function () {
         console.log(newDate)
         return newDate
       },
-
-    clearMealList: function () {
-      document.querySelector(UISelectors.mealList).innerHTML = '';
-    },
 
     clearFormInput: function () {
       document.querySelector(UISelectors.date).value = '';
@@ -146,19 +87,6 @@ const UICtrl = (function () {
       document.querySelector(UISelectors.backBtn).style.display = 'none';
     },
 
-    // toggleClearBtn: function () {
-    //   // Enable clear btn 
-    //   if (document.querySelector(UISelectors.clearBtn).disabled) {
-    //     document.querySelector(UISelectors.clearBtn).disabled = false;
-
-    //     return true;
-    //   } else {
-    //     document.querySelector(UISelectors.clearBtn).disabled = true;
-
-    //     return false;
-    //   }
-    // },
-
     showEditState: function () {
       // Change form Title
       document.querySelector(UISelectors.formTitle).innerText = 'Edit Meal';
@@ -168,19 +96,12 @@ const UICtrl = (function () {
       document.querySelector(UISelectors.deleteBtn).style.display = 'inline-block';
       document.querySelector(UISelectors.backBtn).style.display = 'inline-block';
     },
-
-    addMealToForm: function () {
-      document.querySelector(UISelectors.date).value = MealCtrl.getCurrentMeal().date;
-      document.querySelector(UISelectors.meal).value = MealCtrl.getCurrentMeal().mealName;
-
-      UICtrl.showEditState();
-    },
   };
 })();
 
 
 // App Controller
-const AppCtrl = (function (StorageCtrl, MealCtrl, UICtrl) {
+const AppCtrl = (function (UICtrl) {
   // Get UI selectors
   const UISelectors = UICtrl.getUISelectors();
 
@@ -188,188 +109,23 @@ const AppCtrl = (function (StorageCtrl, MealCtrl, UICtrl) {
   const loadEventListeners = function () {
 
 
-    // Add Meal events
-    document.querySelector(UISelectors.addBtn).addEventListener('click', mealAddSubmit);
-
-    // Disable submit on enter
-    document.addEventListener('keypress', function (e) {
-      if (e.keyCode === 13 || e.which === 13) {
-        e.preventDefault();
-        return false;
+    // Toggle edit state
+    document.querySelector(UISelectors.mealCard).addEventListener('click', function (e) {
+      if(e.target.classList.contains('fa-edit')) {
+        if(document.querySelector(UISelectors.formTitle).innerText === 'Add Meal') {
+          UICtrl.showEditState();
+        } else if(document.querySelector(UISelectors.formTitle).innerText === 'Edit Meal') {
+          UICtrl.clearEditState()
+        }
       }
-    });
 
-
-    document.querySelector(UISelectors.icons).addEventListener()
-
-
-    // Edit meal  events
-    document.querySelector(UISelectors.mealList).addEventListener('click', editMeal);
-
-    // Update meal info events
-    document.querySelector(UISelectors.updateBtn).addEventListener('click', UpdateMealSubmit);
-
-    // Delete meal info events
-    document.querySelector(UISelectors.deleteBtn).addEventListener('click', deleteMealSubmit);
+      e.preventDefault();
+    })
 
     // back btn events
     document.querySelector(UISelectors.backBtn).addEventListener('click', backBtnSubmit);
-
-    // Clear-all btn event
-  //   document.querySelector(UISelectors.clearBtn).addEventListener('click', clearAllMealClick);
   };
 
-  // Add contact on Submit
-  const mealAddSubmit = function (e) {
-
-    // validate Form
-    if (UICtrl.validateForm() !== false) {
-      // Clear Alert
-      UICtrl.clearAlert(UISelectors.alert);
-
-      // Add new Contact to data structure
-      const input = UICtrl.getFormInputs();
-      const newMeal = MealCtrl.addMeal(input);
-
-      // Store in localStorage
-      StorageCtrl.storeMeal(newMeal);
-
-      // Get all meals
-      const mealOptions = StorageCtrl.getMealOptionsFromLs();
-      // show meal list header
-      document.querySelector(UISelectors.displayTitle).style.display = 'block';
-
-      // Populate meal List
-      UICtrl.populateMealDisplay(mealOptions);
-      UICtrl.updateMealCount();
-
-      // Clear Input fields
-      UICtrl.clearFormInput();
-
-      // // Enable clear btn    
-      // UICtrl.toggleClearBtn();
-
-    } else {
-      UICtrl.showAlert(alert, 'Please fill all fields!', 'alert alert-danger');
-    }
-
-    e.preventDefault();
-  };
-
-
-  // Edit meal on click
-  const editMeal = function (e) {
-
-    if (e.target.classList.contains('edit-meal')) {
-
-      // // Disable clear-all
-      // UICtrl.toggleClearBtn();
-
-      // Get contact id
-      const mealId = e.target.parentNode.id;
-
-      // Break into an array
-      const mealIdArr = mealId.split('-');
-
-      // Get the actual id
-      const id = parseInt(mealIdArr[1]);
-
-      // Get meal
-      const mealToEdit = MealCtrl.getMealById(id);
-      console.log(mealToEdit)
-
-      // Set current meal
-      MealCtrl.setCurrentMeal(mealToEdit);
-
-      // Add meal to form
-      UICtrl.addMealToForm();
-    }
-
-    e.preventDefault();
-  };
-
-
-  // Update meal on submit
-  const UpdateMealSubmit = function (e) {
-    // validate Form
-    if (UICtrl.validateForm()) { // Clear Alert
-      UICtrl.clearAlert(UISelectors.alert);
-
-      // Get meal input
-      const meal = UICtrl.getFormInputs();
-
-      console.log(MealCtrl.logData().currentMeal)
-      console.log(meal, 'meals')
-
-      // Update current meal
-      const currentMeal = MealCtrl.getCurrentMeal()
-      console.log(currentMeal, 'initial')
-
-      currentMeal.date = meal.date;
-      currentMeal.mealName = meal.meal;
-      console.log(currentMeal, 'final')
-
-      // Update Ls
-      StorageCtrl.updateMealLs(currentMeal);
-      
-
-      // clear meal list
-      UICtrl.clearMealList();
-
-      // get meal option
-      const mealOptions = MealCtrl.getData();
-      console.log(mealOptions);
-
-      // populate meal list
-      UICtrl.populateMealDisplay(mealOptions);
-
-      UICtrl.showAlert(UISelectors.alert, 'Meal updated!', 'alert alert-success');
-
-      UICtrl.clearEditState();
-
-      UICtrl.clearFormInput()
-
-    } else {
-      showAlert(UICtrl.alert, 'Please fill all fields!', 'alert alert-success');
-    }
-
-    UICtrl.clearAlert(UISelectors.alert);
-    e.preventDefault();
-  };
-
-  // Delete meal on submit
-  const deleteMealSubmit = function (e) {
-    // Get current meal
-    const currentMeal = MealCtrl.getCurrentMeal();
-
-    //Delete from Ls
-    StorageCtrl.deleteMealFromLs(currentMeal.id);
-
-    // Delete from data structure
-    MealCtrl.deleteMeal(currentMeal.id);
-
-    UICtrl.updateMealCount();
-
-    // Clear Edit State
-    UICtrl.clearEditState();
-
-    // get meal option
-    const mealOptions = MealCtrl.getData();
-
-    if(mealOptions.length === 0) {
-      document.querySelector(UISelectors.displayContainer).style.display = 'none';
-    }
-
-    // populate meal list
-    UICtrl.populateMealDisplay(mealOptions);
-
-    UICtrl.showAlert(UISelectors.alert, 'Meal deleted!', 'alert alert-success');
-
-    UICtrl.clearAlert(UISelectors.alert);
-
-
-    e.preventDefault();
-  };
 
   const backBtnSubmit = function (e) {
 
@@ -379,25 +135,6 @@ const AppCtrl = (function (StorageCtrl, MealCtrl, UICtrl) {
 
     e.preventDefault();
   };
-
-  const clearAllMealClick = function () {
-    // Delete all meal options from data structure
-    UICtrl.clearAllMealOptions();
-
-    // Clear all contacts from UI
-    UICtrl.clearMealList();
-
-    // Clear all contacts from Ls
-    StorageCtrl.clearAllMealsFromLs();
-
-    // Update contacts counter
-    UICtrl.updateMealCount();
-
-    // Disable clear-all btn
-    UICtrl.toggleClearBtn();
-
-  };
-
 
   // Public methods
   return {
@@ -419,7 +156,7 @@ const AppCtrl = (function (StorageCtrl, MealCtrl, UICtrl) {
     }
   };
 
-})(StorageCtrl, MealCtrl, UICtrl);
+})(UICtrl);
 
 
 
