738f869e3f6cfabe734984429ba3cd73546008bc
rovilay <rovilayjnr01@gmail.com>
2018-04-26 19:16:49 +0100

    Add post menu routes [updates #156831141]

--- server/routes/menuRoutes.js
+++ server/routes/menuRoutes.js
@@ -5,6 +5,7 @@ const menuRouter = express.Router();
 
 menuRouter.get('/api/v1/menus', menusController.getAllMenus);
 menuRouter.get('/api/v1/menus/:DD/:MM/:YYYY', menusController.getMenu);
+menuRouter.post('/api/v1/menus', menusController.postMenu);
 
 
 export default menuRouter;
