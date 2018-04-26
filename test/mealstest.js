e336e459d33fe303523d5df97ec9180604150255
rovilay <rovilayjnr01@gmail.com>
2018-04-26 04:47:42 +0100

    Update tests for get single meal api [updates #157108762]

--- test/mealstest.js
+++ test/mealstest.js
@@ -13,48 +13,6 @@ describe('GET /api/v1/meals', () => {
   it('should return status code 200', (done) => {
 
     chai.request(app.listen())
-      .get('/api/v1/meals/1')
-      .end((err, res) => {
-        // console.log(res);
-        res.status.should.equal(200);
-        expect('content-type', /json/);
-        res.body.should.have.property('success');
-        res.body.should.have.property('message');
-        res.body.should.have.property('meal');
-        res.body.meal.should.be.an('object');
-        res.body.meal.should.not.be.an('array');
-        done();
-      });
-  });
-  it('Meal returned should have right properties', (done) => {
-
-    chai.request(app.listen())
-      .get('/api/v1/meals/1')
-      .end((err, res) => {
-          meal.should.be.an('object');
-          meal.should.have.property('id');
-          meal.should.have.property('title');
-          meal.should.have.property('description');
-          meal.should.have.property('price');
-          meal.should.have.property('img');
-          meal.id.should.be.a('number');
-          meal.title.should.be.a('string');
-          meal.description.should.be.a('string');
-          parseInt(meal.price).should.be.a('number');
-          if(meal.img !== null) {
-            meal.img.should.be.a('string');
-          }
-        
-        done();
-      });
-  });
-  
-});
-describe('GET /api/v1/meals', () => {
-
-  it('should return status code 200', (done) => {
-
-    chai.request(app.listen())
       .get('/api/v1/meals')
       .end((err, res) => {
         // console.log(res);
@@ -86,6 +44,58 @@ describe('GET /api/v1/meals', () => {
           parseInt(meal.price).should.be.a('number');
 
         });
+        
+        done();
+      });
+  });
+  
+});
+
+describe('GET /api/v1/meals/:id', () => {
+
+  it('should return status code 200', (done) => {
+
+    chai.request(app.listen())
+      .get('/api/v1/meals/1')
+      .end((err, res) => {
+        // console.log(res);
+        res.status.should.equal(200);
+        expect('content-type', /json/);
+        res.body.should.have.property('success');
+        res.body.should.have.property('message');
+        res.body.should.have.property('meal');
+        res.body.meal.should.be.an('object');
+        res.body.meal.should.not.be.an('array');
+        done();
+      });
+  });
+  it('Meal returned should have right properties if id exist', (done) => {
+
+    chai.request(app.listen())
+      .get('/api/v1/meals/1')
+      .end((err, res) => {
+          res.meal.should.be.an('object');
+          res.meal.should.have.property('id');
+          res.meal.should.have.property('title');
+          res.meal.should.have.property('description');
+          res.meal.should.have.property('price');
+          res.meal.should.have.property('img');
+          res.meal.id.should.be.a('number');
+          res.meal.title.should.be.a('string');
+          res.meal.description.should.be.a('string');
+          parseInt(meal.price).should.be.a('number');
+          if(res.meal.img !== null) {
+            res.meal.img.should.be.a('string');
+          }
+        
+        done();
+      });
+  });
+  it('should return 404 for wrong id', (done) => {
+    chai.request(app.listen())
+      .get('/api/v1/meals/15')
+      .end((err, res) => {
+        res.status.should.equal(404);
         
         done();
       });
