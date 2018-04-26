86a11dc0cd7b7d9b8d4b7fa5ae3dcca70b195b98
rovilay <rovilayjnr01@gmail.com>
2018-04-26 06:09:47 +0100

    Write test for add meal request [start #157108783]

--- server/controller/meals.js
+++ server/controller/meals.js
@@ -26,6 +26,38 @@ class MealsController {
       message: `meal with id ${id} does not exist!`
     });
   }
+
+  addMeal(req, res) {
+    if (!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is empty'
+      });
+    } else if (!req.body.description || !req.body.price) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'some fields are empty'
+      });
+    }
+    // obj to input to the db
+    const meal = {
+      id: parseInt(meals[meals.length - 1].id) + 1,
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.body.image
+    };
+
+    // push meal to db
+    meals.push(meal);
+
+    return res.status(201).send({
+      success: 'true',
+      message: 'Meal added successfully',
+      meals
+    });
+  }
+
 }
 
 const mealsController = new MealsController();
--- test/mealstest.js
+++ test/mealstest.js
@@ -103,3 +103,104 @@ describe('GET /api/v1/meals', () => {
   
 });
 
+describe('POST /api/v1/meals', () => {
+
+  it('should return status code 400', (done) => {
+
+    chai.request(app.listen())
+      .post('/api/v1/meals')
+      .send({
+        title: 'Rice and Chicken',
+        description: 'Very Good',
+        price: 700,
+        img: 'hp.image.com'
+      })
+      .end((err, res) => {
+        // console.log(res);
+        res.status.should.equal(400);
+        // expect('content-type', /json/);
+        // res.body.should.have.property('success');
+        // res.body.should.have.property('message');
+        // res.body.should.have.property('meals');
+        // res.body.meals.should.be.an('array');
+        // res.body.meals[0].should.be.an('object');
+        done();
+      });
+  });
+  // it('Meals returned should have right properties', (done) => {
+
+  //   chai.request(app.listen())
+  //     .get('/api/v1/meals')
+  //     .end((err, res) => {
+  //       res.body.meals.forEach(meal => {
+  //         meal.should.be.an('object');
+  //         meal.should.have.property('id');
+  //         meal.should.have.property('title');
+  //         meal.should.have.property('description');
+  //         meal.should.have.property('price');
+  //         meal.should.have.property('img');
+  //         meal.id.should.be.a('number');
+  //         meal.title.should.be.a('string');
+  //         meal.description.should.be.a('string');
+  //         parseInt(meal.price).should.be.a('number');
+
+  //       });
+        
+  //       done();
+  //     });
+  // });
+
+  // describe('GET /api/v1/meals/:id', () => {
+
+  //   it('should return status code 200', (done) => {
+  
+  //     chai.request(app.listen())
+  //       .get('/api/v1/meals/1')
+  //       .end((err, res) => {
+  //         // console.log(res);
+  //         res.status.should.equal(200);
+  //         expect('content-type', /json/);
+  //         res.body.should.have.property('success');
+  //         res.body.should.have.property('message');
+  //         res.body.should.have.property('meal');
+  //         res.body.meal.should.be.an('object');
+  //         res.body.meal.should.not.be.an('array');
+  //         done();
+  //       });
+  //   });
+  //   it('Meal returned should have right properties if id exist', (done) => {
+  //     chai.request(app.listen())
+  //       .get('/api/v1/meals/1')
+  //       .end((err, res) => {
+  //         console.log(res.meal);
+  //           res.body.meal.should.be.an('object');
+  //           res.body.meal.should.have.property('id');
+  //           res.body.meal.should.have.property('title');
+  //           res.body.meal.should.have.property('description');
+  //           res.body.meal.should.have.property('price');
+  //           res.body.meal.should.have.property('img');
+  //           res.body.meal.id.should.be.a('number');
+  //           res.body.meal.title.should.be.a('string');
+  //           res.body.meal.description.should.be.a('string');
+  //           parseInt(res.body.meal.price).should.be.a('number');
+  //           if(res.body.meal.img !== null) {
+  //             res.body.meal.img.should.be.a('string');
+  //           }
+          
+  //         done();
+  //       });
+  //   });
+  //   it('should return 404 if id do not exist', (done) => {
+  //     chai.request(app.listen())
+  //       .get('/api/v1/meals/15')
+  //       .end((err, res) => {
+  //         res.status.should.equal(404);
+          
+  //         done();
+  //       });
+  //   });
+    
+  // });
+  
+});
+
