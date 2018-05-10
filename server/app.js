83d321f73c09f6ee12d8da4a9fd5f71bb7d21f20
rovilay <rovilayjnr01@gmail.com>
2018-05-10 14:00:11 +0100

    update start script

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import usersRouter from './routes/usersRoutes';
 import authorize from './middlewares/authenticate';
 
 
-require('dotenv').config();
+require('dotenv').config(); //
 
 const app = express();
 const port = process.env.PORT || 4000;
