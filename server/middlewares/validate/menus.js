02c8de3687b32f5dc92e8224cd490b5b97a328f2
rovilay <rovilayjnr01@gmail.com>
2018-05-22 00:04:54 +0100

    Add date validation

--- server/middlewares/validate/menus.js
+++ server/middlewares/validate/menus.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 /**
  * Validates sent menu inputs
  *
@@ -19,6 +21,14 @@ export function validateMenu(req, res, next) {
       return next(err);
     }
   });
+
+  if (!moment(req.body.postOn, 'YYYY-MM-DD', true).isValid()) {
+    const err = new Error('postOn input is invalid');
+    err.status = 400;
+    return next(err);
+  }
+
+
   return next();
 }
 
@@ -38,6 +48,33 @@ export function validateUpdateMenu(req, res, next) {
     const err = new Error(`${key} field is empty`);
     err.status = 400;
     return next(err);
+  }
+
+  return next();
+}
+
+/**
+ * Validate date on request parameters(../:DD/:MM/:YYYY)
+ *
+ * @export validateParams
+ * @param  {object} req - Request object
+ * @param  {object} res - Response object
+ * @param  {object} next - next object (handles error or continues to next
+ * middleware)
+ * @return {object} next
+ */
+export function validateParams(req, res, next) {
+  if (req.params.DD && req.params.MM && req.params.YYYY) {
+    const day = req.params.DD;
+    const month = req.params.MM;
+    const year = req.params.YYYY;
+    const date = `${year}-${month}-${day}`;
+
+    if (moment(date, 'YYYY-MM-DD', true).isValid() === false) {
+      const err = new Error("'/DD/MM/YYYY' parameters is invalid!'");
+      err.status = 400;
+      return next(err);
+    }
   }
 
   return next();
--- server/routes/menuRoutes.js
+++ server/routes/menuRoutes.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import menusController from '../controller/menus';
 import adminOnly from '../middlewares/adminOnly';
-import { validateMenu, validateUpdateMenu } from '../middlewares/validate/menus';
+import { validateMenu, validateUpdateMenu, validateParams } from '../middlewares/validate/menus';
 
 const menuRouter = express.Router();
 
-menuRouter.get('/api/v1/menus/:DD/:MM/:YYYY', menusController.getMenu);
+menuRouter.get('/api/v1/menus/:DD/:MM/:YYYY', validateParams, menusController.getMenu);
 
 menuRouter.get('/api/v1/menus', adminOnly, menusController.getAllMenus);
 
 menuRouter.post('/api/v1/menus', adminOnly, validateMenu, menusController.postMenu);
 
-menuRouter.put('/api/v1/menus/:DD/:MM/:YYYY', adminOnly, validateUpdateMenu, menusController.updateMenu);
+menuRouter.put('/api/v1/menus/:DD/:MM/:YYYY', adminOnly, validateParams, validateUpdateMenu, menusController.updateMenu);
 
 
 export default menuRouter;
