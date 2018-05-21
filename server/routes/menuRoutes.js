eb2e0c7ce6c6aebbe584c5efe9930117801c291d
rovilay <rovilayjnr01@gmail.com>
2018-05-21 22:57:20 +0100

    Add get orders by user id routes and controller [#157756912]

--- server/routes/menuRoutes.js
+++ server/routes/menuRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import menusController from '../controller/menus';
 import adminOnly from '../middlewares/adminOnly';
-import validateMenu from '../middlewares/validate/menus';
+import { validateMenu, validateUpdateMenu } from '../middlewares/validate/menus';
 
 const menuRouter = express.Router();
 
@@ -10,6 +10,8 @@ menuRouter.get('/api/v1/menus/:DD/:MM/:YYYY', menusController.getMenu);
 menuRouter.get('/api/v1/menus', adminOnly, menusController.getAllMenus);
 
 menuRouter.post('/api/v1/menus', adminOnly, validateMenu, menusController.postMenu);
+
+menuRouter.put('/api/v1/menus/:DD/:MM/:YYYY', adminOnly, validateUpdateMenu, menusController.updateMenu);
 
 
 export default menuRouter;
