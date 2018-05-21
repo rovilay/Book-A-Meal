3d0123e65eb6464f173ffc734fdab83e9a8ac331
rovilay <rovilayjnr01@gmail.com>
2018-05-21 22:52:29 +0100

    Fix: menu meals validation issue [#157757192]

--- server/middlewares/validate/menus.js
+++ server/middlewares/validate/menus.js
@@ -1,14 +1,14 @@
 /**
  * Validates sent menu inputs
  *
- * @exports validatemenu
+ * @export validatemenu
  * @param  {object} req - Request object
  * @param  {object} res - Response object
  * @param  {object} next - next object (handles error or continues to next
  * middleware)
  * @return {object} next
  */
-function validatemenu(req, res, next) {
+export function validateMenu(req, res, next) {
   const keys = ['postOn', 'meals'];
 
   keys.forEach((key) => {
@@ -22,4 +22,24 @@ function validatemenu(req, res, next) {
   return next();
 }
 
-export default validatemenu;
+/**
+ * Validate menu update inputs
+ *
+ * @export validateUpdateMenu
+ * @param  {object} req - Request object
+ * @param  {object} res - Response object
+ * @param  {object} next - next object (handles error or continues to next
+ * middleware)
+ * @return {object} next
+ */
+export function validateUpdateMenu(req, res, next) {
+  const key = 'meals';
+  if (req.body[`${key}`] === undefined || req.body[`${key}`] === '') {
+    const err = new Error(`${key} field is empty`);
+    err.status = 400;
+    return next(err);
+  }
+
+  return next();
+}
+
