c5c1f20c9966db5538bade63e490bfb9b71cdf44
rovilay <rovilayjnr01@gmail.com>
2018-06-02 16:10:18 +0100

    Modify Headr component to be more dynamic [#158046771]

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
+import { delFromLs } from '../../helpers/Ls';
+import { emptyCart } from '../../actions/cart';
+import { setDefaultNav } from '../../actions/navLinks';
+
 const Header = (props) => {
-  const { navLinks } = props;
+  const { navLinks, history, dispatch } = props;
+
+  const onLogOut = () => {
+    delFromLs('jwt');
+    dispatch(emptyCart());
+    dispatch(setDefaultNav());
+    history.push('/');
+  };
   return (
     <header>
       <div className="header">
@@ -12,13 +24,27 @@ const Header = (props) => {
         <div className="header-right">
           {navLinks &&
             navLinks.map((nav, i) => (
-              <NavLink
-                to={nav.link}
-                key={i}
-                className="is-active"
-              >
-                {nav.title}
-              </NavLink>
+              (nav.title === 'Log Out')
+              ?
+              (
+                <button
+                  className="btn"
+                  onClick={onLogOut}
+                  key={i}
+                >
+                  {nav.title}
+                </button>
+              )
+              :
+              (
+                <NavLink
+                  to={nav.link}
+                  key={i}
+                  className="is-active"
+                >
+                  {nav.title}
+                </NavLink>
+              )
             ))}
         </div>
       </div>
@@ -27,11 +53,13 @@ const Header = (props) => {
 };
 
 Header.propTypes = {
-  navLinks: PropTypes.array.isRequired
+  navLinks: PropTypes.array.isRequired,
+  history: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   navLinks: state.navLinks
 });
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(withRouter(Header));
