d8e7f4f4d43c48bc148daa382bc896eeb9da7c2c
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-09 13:00:56 +0100

    [#157129335]

--- server/routes/mealRoutes.js
+++ server/routes/mealRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import mealController from '../controller/meals';
 import validateMeal from '../middlewares/validate/meals';
-
+import authorize from '../middlewares/authenticate';
 import adminOnly from '../middlewares/adminOnly';
 
 const mealRouter = express.Router();
 
-
+mealRouter.use(authorize);
 mealRouter.get('/api/v1/meals', adminOnly, mealController.getAllMeals);
 mealRouter.get('/api/v1/meals/:id', adminOnly, mealController.getMeal);
 mealRouter.delete('/api/v1/meals/:id', adminOnly, mealController.deleteMeal);
