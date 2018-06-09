99af2602fbf427bb394b36759653a2b6c9603ef9
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-09 13:22:11 +0100

    Update component to use data from redux store [#158235584]

--- client/src/components/HOCs/chooseDashboard.js
+++ client/src/components/HOCs/chooseDashboard.js
@@ -1,53 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import jwt from 'jsonwebtoken';
 import PropTypes from 'prop-types';
 
 import isExpired from '../../helpers/isExpired';
-import { setDefaultNav } from '../../actions/navLinks';
 import { getFromLs } from '../../helpers/Ls';
+import { setDefaultNav } from '../../actions/navLinks';
 
 export default function (CompA, CompB) {
   class ChooseDashboard extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        admin: false,
-        expire: '',
         token: ''
       };
     }
 
     componentWillMount() {
       const { history, dispatch } = this.props;
+      const { expire } = this.props.user;
       const token = getFromLs('jwt');
-      if (token) {
-        const {
-          admin,
-          exp: expire
-        } = jwt.decode(token);
-
-        this.setState({
-          admin,
-          expire,
-          token
-        });
-      } else {
-        history.push('/login');
-        dispatch(setDefaultNav());
-      }
-
-      const { expire } = this.state;
 
       if (isExpired(expire)) {
-        history.push('/');
         dispatch(setDefaultNav());
+        return history.push('/login');
       }
+
+      this.setState({ token });
     }
 
     render() {
-      const { admin, token } = this.state;
+      const { token } = this.state;
+      const { admin } = this.props.user;
       return (
         <div>
           {
@@ -64,7 +48,8 @@ export default function (CompA, CompB) {
   ChooseDashboard.propTypes = {
     history: PropTypes.object.isRequired,
     dispatch: PropTypes.func.isRequired,
-    todayMenu: PropTypes.array.isRequired
+    todayMenu: PropTypes.array.isRequired,
+    user: PropTypes.object.isRequired
   };
 
   const mapStateToProps = state => ({
