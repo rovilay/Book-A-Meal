9695916017b5f17e4338c57ba20c8a826409631e
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-15 17:45:53 +0100

    Fix: lagging get order request

--- client/src/actions/orders.js
+++ client/src/actions/orders.js
@@ -1,10 +1,5 @@
 import serverReq from '../helpers/serverReq';
 
-// export const deleteOrder = id => ({
-//   type: 'DELETE_ORDER',
-//   mealId: id
-// });
-
 /**
  * Redux action that sets Customer orders in redux store
  *
@@ -26,6 +21,12 @@ export const setCustomerOrders = ({
   }
 });
 
+/**
+ * Redux action that sets order server response to store
+ *
+ * @param {Object}  - object with property success and message
+ * @returns {Object} - returns object of action type and serverRes
+ */
 const orderServerRes = ({ success, message }) => ({
   type: 'ORDER_SERVER_RES',
   serverRes: {
@@ -34,6 +35,12 @@ const orderServerRes = ({ success, message }) => ({
   }
 });
 
+/**
+ * Redux action that sets edit order to store
+ *
+ * @param {Object}  - object with properties orderId,deliveryAddress, orderedMeals and totalprice
+ * @returns {Object} - returns object of action type and editOrder properties
+ */
 export const setEditOrder = ({
   orderId,
   deliveryAddress,
@@ -49,6 +56,12 @@ export const setEditOrder = ({
   }
 });
 
+/**
+ * Redux action that update order meal portion in store
+ *
+ * @param {Object}  - object with property mealId and portion
+ * @returns {Object} - returns object of action type and meal properties
+ */
 export const updateMealPortion = ({
   mealId,
   portion
@@ -60,15 +73,53 @@ export const updateMealPortion = ({
   }
 });
 
-export const updateDelAddress = deliveryAddress => ({
-  type: 'UPDATE_DELIVERY_ADDRESS',
-  address: deliveryAddress
-});
+// /**
+//  *
+//  * @param {string} deliveryAddress - address of meal to delete
+//  * @returns {Object} - returns object of action type and mealId properties
+//  */
+// export const updateDelAddress = deliveryAddress => ({
+//   type: 'UPDATE_DELIVERY_ADDRESS',
+//   address: deliveryAddress
+// });
 
+/**
+ * Redux action that delete ordered meal in store
+ *
+ * @param {string} id - Id of meal to delete
+ * @returns {Object} - returns object of action type and mealId
+ */
 export const deleteMealInEditOrder = id => ({
   type: 'DEL_MEAL_EDIT_ORDER',
   mealId: id
 });
+
+/**
+ * Sends async server requests to get orders using the axios api
+ *
+ * @param  {string} userId - Id of user to get order
+ * @return {Function} - dispatches an set customer order action to the redux store
+ */
+export const getOrders = userId => (dispatch) => {
+  serverReq('get', `/api/v1/orders/${userId}`)
+    .then((res) => {
+      const {
+        success,
+        message,
+        orders,
+        grandTotalPrice
+      } = res.data;
+      dispatch(setCustomerOrders({
+        success,
+        message,
+        orders,
+        grandTotalPrice
+      }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 /**
  * Sends async server requests to update order using the axios api
