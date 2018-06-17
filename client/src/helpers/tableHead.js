da414d0f3d6ab5201c0cb2d198d0b4798e7679e7
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-17 10:32:54 +0100

    Add menu details table head data  [#158410667]

--- client/src/helpers/tableHead.js
+++ client/src/helpers/tableHead.js
@@ -1,15 +1,17 @@
-const cartTableHead = ['S/N', 'Meal', 'UnitPrice', 'Portion', 'Price', 'Actions'];
-const editOrderTableHead = ['S/N', 'Meal', 'UnitPrice', 'Portion', 'Portion', 'Actions'];
+const cartTableHead = ['S/N', 'Meal', 'Unit Price', 'Portion', 'Price', 'Actions'];
+const editOrderTableHead = ['S/N', 'Meal', 'Unit Price', 'Portion', 'Portion', 'Actions'];
 const customerOrderHead = ['S/N', 'OrderId', 'Date', 'Total Price', 'Actions'];
 const orderDetailHead = ['S/N', 'Meal', 'Unit Price', 'Portion', 'Price'];
 const menuTableHead = ['S/N', 'Menu Id', 'Post On', 'Created By', 'Actions'];
+const menuDetailHead = ['S/N', 'Meal Id', 'Meal', 'Unit Price', 'Description'];
 
 const tableHead = {
   cartTableHead,
   customerOrderHead,
   orderDetailHead,
   editOrderTableHead,
-  menuTableHead
+  menuTableHead,
+  menuDetailHead
 };
 
 export default tableHead;
