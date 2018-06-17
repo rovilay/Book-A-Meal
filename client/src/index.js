3a332389e09fe17f618ee73334647db932d59f5d
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-17 10:30:38 +0100

    Add static table head and scroll to table body [#158410667]

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { persistor, store } from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 import './assets/scss/style.scss';
 import './assets/css/create-menu.css';
+import './assets/css/table.css';
 
 // store.subscribe(() => {
 //   const state = store.getState();
