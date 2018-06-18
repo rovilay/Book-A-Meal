4c89551ee7277f73b30a2b93a185b59da2c4aa77
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-18 19:08:37 +0100

    Add meal table head [#158417364]

--- client/src/helpers/tableHead.js
+++ client/src/helpers/tableHead.js
@@ -3,6 +3,7 @@ const editOrderTableHead = ['S/N', 'Meal', 'Unit Price', 'Portion', 'Portion', '
 const customerOrderHead = ['S/N', 'OrderId', 'Date', 'Total Price', 'Actions'];
 const orderDetailHead = ['S/N', 'Meal', 'Unit Price', 'Portion', 'Price'];
 const menuTableHead = ['S/N', 'Menu Id', 'Post On', 'Created By', 'Actions'];
+const mealTableHead = ['S/N', 'Meal', 'Unit Price', 'Description', 'Created On', 'Actions'];
 const menuDetailHead = ['S/N', 'Meal Id', 'Meal', 'Unit Price', 'Description'];
 const editMenuHead = ['S/N', 'Meal', 'Unit Price', 'Description', 'Actions'];
 
@@ -13,7 +14,8 @@ const tableHead = {
   editOrderTableHead,
   menuTableHead,
   menuDetailHead,
-  editMenuHead
+  editMenuHead,
+  mealTableHead
 };
 
 export default tableHead;
