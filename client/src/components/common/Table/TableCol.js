d9bb6f3684a57748c04a88a817bc2b365aa6243c
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-06-25 00:13:00 +0100

    Add image tool tip to meals [#158417364]

--- client/src/components/common/Table/TableCol.js
+++ client/src/components/common/Table/TableCol.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classname from 'classnames';
 
 const TableCol = props => (
-  <td data-title={props.dataTitle}>
+  <td data-title={props.dataTitle} className={classname('', { 'meal-Image': props.dataTitle === 'meal' })}>
     {props.val}
+    {
+      (props.dataTitle === 'meal')
+      &&
+      (
+        <span className="tooltip">
+          <img src={props.mealImage} alt={props.val} />
+        </span>
+      )
+    }
   </td>
 );
 
+TableCol.defaultProps = {
+  mealImage: undefined
+};
+
 TableCol.propTypes = {
   dataTitle: PropTypes.string.isRequired,
-  val: PropTypes.any.isRequired
+  val: PropTypes.any.isRequired,
+  mealImage: PropTypes.string
 };
 
 export default TableCol;
